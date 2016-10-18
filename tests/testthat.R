9c2d6b9051a21f23dbb6b4148ccc666527267332
wilcoxa <jumping172@gmail.com>
2016-10-18 16:20:03 +1100

    LC_COLLATE = "C", added some travis debugging

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,10 @@
 Sys.setenv("R_TESTS" = "")
 # Sys.setenv(LC_COLLATE = "C", LC_TIME = "C", LANGUAGE = "en")
-Sys.setenv(LC_COLLATE = "en_US.UTF-8")
+Sys.setenv(LC_COLLATE = "C")
 
 print(Sys.getlocale(category = "LC_ALL"))
-# Sys.setlocale("LC_COLLATE", "C") # R CMD check uses this default
-Sys.setlocale("LC_COLLATE", "en_US.UTF-8")
+Sys.setlocale("LC_COLLATE", "C") # R CMD check uses this default
+# Sys.setlocale("LC_COLLATE", "en_US.UTF-8")
 # Sys.setlocale("LC_COLLATE", "English_United States.1252")
 print(Sys.getlocale(category = "LC_ALL"))
 
--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -3,6 +3,13 @@ library(foreign)
 options(frequencies_open_output = FALSE)
 options(frequencies_output_flextables = FALSE)
 
+print(Sys.getlocale(category = "LC_ALL"))
+Sys.setlocale("LC_COLLATE", "C") # R CMD check uses this default
+# Sys.setlocale("LC_COLLATE", "en_US.UTF-8")
+# Sys.setlocale("LC_COLLATE", "English_United States.1252")
+print(Sys.getlocale(category = "LC_ALL"))
+
+
 test_spss <- "test_spss.sav"
 
 haven_df <- read_sav(test_spss)
@@ -17,10 +24,20 @@ source("output.R")
 context("Data attributes assumptions")
 
 test_that("Loading foreign", {
+
+  print(foreign_df)
+  print(str(foreign_df))
+  print(raw_foreign_df)
+  print(str(raw_foreign_df))
   expect_equal(foreign_df, raw_foreign_df)
   expect_equal(foreign_list, raw_foreign_list)
 })
 test_that("Loading haven", {
+
+  print(haven_df)
+  print(str(haven_df))
+  print(raw_haven_df)
+  print(str(raw_haven_df))
   expect_equal(haven_df, raw_haven_df)
 })
 
@@ -32,10 +49,30 @@ test_that("All", {
 })
 test_that("Individual tables", {
   expect_equal(raw_id, freq(dat$id))
+
+  print(raw_numeric)
+  print(freq(dat$test_numeric))
+  print(raw_numeric)
+  print(freq(dat$test_numeric))
   expect_equal(raw_numeric, freq(dat$test_numeric))
+
+  print(raw_numeric_labelled)
+  print(freq(dat$test_numeric_labelled))
+  print(raw_numeric_labelled)
+  print(freq(dat$test_numeric_labelled))
   expect_equal(raw_numeric_labelled, freq(dat$test_numeric_labelled))
+
+  print(raw_character)
+  print(freq(dat$test_character))
+  print(raw_character)
+  print(freq(dat$test_character))
   expect_equal(raw_character, freq(dat$test_character))
-  expect_equal(raw_character_labelled, freq(dat$test_character_labelled))
+
+  print(raw_character)
+  print(freq(dat$test_character_labelled))
+  print(raw_character)
+  print(freq(dat$test_character_labelled))
+    expect_equal(raw_character_labelled, freq(dat$test_character_labelled))
 })
 
 #-------------------------------------------------------------------------------
