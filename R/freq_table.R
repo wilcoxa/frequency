498c9f14c032834c12a4aa35b8dcaf183ca7b6f1
wilcoxa <8096628+wilcoxa@users.noreply.github.com>
2020-04-05 17:03:41 +1000

    Add documentation for freq_table print methods.

--- R/freq_table.R
+++ R/freq_table.R
@@ -1,5 +1,9 @@
 #' Print frequency tables
 #'
+#' S3 method for class 'freq_table'
+#'
+#' @param x object of class \code{freq.table}
+#' @param ... optional arguments to \code{data.frame}
 print.freq_table <- function(x, ...){
   print.data.frame(x, ..., row.names = FALSE)
 }
