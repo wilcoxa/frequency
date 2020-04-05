43b8b09164062fe54d9e87cc7573422672f32fb7
wilcoxa <8096628+wilcoxa@users.noreply.github.com>
2020-04-05 17:37:03 +1000

    Add example for print.freq_table

--- R/freq_table.R
+++ R/freq_table.R
@@ -4,6 +4,16 @@
 #'
 #' @param x object of class \code{freq.table}
 #' @param ... optional arguments to \code{data.frame}
+#'
+#' @example
+#' # Suppress external output for examples
+#' options(frequency_render = FALSE)
+#'
+#' x <- freq(big5[1])
+#' print(x)
+#' print(x[[1]])
+#'
+#' @export
 print.freq_table <- function(x, ...){
   print.data.frame(x, ..., row.names = FALSE)
 }
